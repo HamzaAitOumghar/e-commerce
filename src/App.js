7c2334bc54a7d2aaca7c270102210fcb0c71fb04
Hamza Ait Oumghar <hamza.aitoumghar.3@gmail.com>
2019-08-18 20:38:04 +0100

    add react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/NavBar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Default from './components/Default';
+import Cart from './components/Cart';
+
+
+
+
 
 function App() {
     return (
         <React.Fragment>
-            hello from app
-        </React.Fragment> 
+            <Navbar />
+            <Switch>
+                <Route path="/details" component={Details}></Route>
+                <Route exact path="/" component={ProductList}></Route>
+                <Route  path="/cart" component={Cart}></Route>
+                <Route component={Default}></Route>
+            </Switch>
+        </React.Fragment>
     );
 };
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import logo from '../logo.svg'
+
 
 export default class NavBar extends Component {
     render() {
         return (
-            <div>
-                <h3>Hello From Navbar</h3>
-            </div>
-        )
+            <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+                {/* 
+                    https://www.iconfinder.com/icons/1243689/call_phone_icon
+                    Creative Commons (Attribution 3.0 Unported);
+                    https://www.iconfinder.com/Makoto_msk 
+                */}
+                <Link to="/">
+                    <img src={logo} alt="test" className="navbar-brand"/>
+                </Link>
+                ul.nav
+            </nav>        
+            )
     }
 }
