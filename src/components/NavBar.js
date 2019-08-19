419f32732dbd25cc9ffa3e67fbfee4aa94e60ff5
Hamza Ait Oumghar <hamza.aitoumghar.3@gmail.com>
2019-08-19 17:12:16 +0100

    update navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../logo.svg'
-
+import styled from 'styled-components';
+import { ButtonContainer } from './Button'
 
 export default class NavBar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-expand-sm  bg-primary navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm  bg-primary navbar-dark px-sm-5">
                 {/*
                 https://www.iconfinder.com/icons/1243689/call_phone_icon
                 Creative Commons (Attribution 3.0 Unported);
                 https://www.iconfinder.com/Makoto_msk 
-        */}
+                */}
                 <Link to='/'>
                     <img className="navbar-brand" alt="logo" src={logo} />
                 </Link>
@@ -21,11 +22,24 @@ export default class NavBar extends Component {
                     </li>
                 </ul>
                 <Link to="/cart" className="ml-auto">
-                    <button className="btn btn-outline-dark">
-                        <i className="fas fa-cart-plus"></i> My Cart
-                </button>
+                    <ButtonContainer >
+                        <span className="mr-2"><i className="fas fa-cart-plus"></i></span>
+                        My Cart
+                    </ButtonContainer>
                 </Link>
-            </nav>
+            </NavWrapper>
         )
     }
 }
+
+
+const NavWrapper = styled.nav`
+     background : var(--mainBlue) !important;
+     .nav-link{
+         color:var(--mainWhite) !important;
+         font-size : 1.3rem;
+         text-transform:capitalize;
+
+     }
+
+`;
